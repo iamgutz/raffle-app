8f6319dc9a4dd7623a6db5a013615e1e285b8893
Michael Gutierrez <13262904+iamgutz@users.noreply.github.com>
2024-07-02 14:18:03 -0600

    fix config

--- src/config/index.js
+++ src/config/index.js
@@ -1,25 +1,27 @@
-export default {
-  forcedWinner: 'John Doe',
-  theme: {
-    appLogoImgSrc: `${process.env.PUBLIC_URL}/images/raffle-logo.jpg`,
-    bgColor: '#F7F7F7',
-    buttonColor: '#FF070C',
-    listItemBgColorEven: '#E7EDED',
-    textColor: '#000834',
-  },
-  text: {
-    addParticipantsTitle: 'Add Participants',
-    backButton: 'Go Back',
-    cancelButton: 'Cancel',
-    confirmButton: 'Confirm & Go!',
-    confirmTitle: 'Confirm the participants',
-    congratulationsSuperTitle: 'Congratulations!',
-    congratulationsTitle: 'The winner is...',
-    continueButton: 'Continue',
-    enterButton: 'Enter now!',
-    finishButton: 'Finalize',
-    pickingWinnerTitle: 'Picking the winner',
-    slogan: 'Win prizes today, good luck.',
-    startOverButton: 'Start Over',
-  },
+const config = {
+    forcedWinner: 'John Doe',
+    theme: {
+        appLogoImgSrc: `${process.env.PUBLIC_URL}/images/raffle-logo.jpg`,
+        bgColor: '#F7F7F7',
+        buttonColor: '#FF070C',
+        listItemBgColorEven: '#E7EDED',
+        textColor: '#000834',
+    },
+    text: {
+        addParticipantsTitle: 'Add Participants',
+        backButton: 'Go Back',
+        cancelButton: 'Cancel',
+        confirmButton: 'Confirm & Go!',
+        confirmTitle: 'Confirm the participants',
+        congratulationsSuperTitle: 'Congratulations!',
+        congratulationsTitle: 'The winner is...',
+        continueButton: 'Continue',
+        enterButton: 'Enter now!',
+        finishButton: 'Finalize',
+        pickingWinnerTitle: 'Picking the winner',
+        slogan: 'Win prizes today, good luck.',
+        startOverButton: 'Start Over',
+    },
 };
+
+export default config;
